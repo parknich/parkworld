49e60f861ddcde30608ae006255df559d557a5fd
parknich081 <45834520+parknich081@users.noreply.github.com>
2023-03-01 08:55:14 -0500

    sadasd

--- index.js
+++ index.js
@@ -3,7 +3,71 @@ char1_posx = null
 char1_posy = null
 char1_sposx = null
 char1_sposy = null
-class movement() {
-    
+class pw extends Phaser.Scene
+{
+    constructor ()
+    {
+        super();
+    }
 
+    preload ()
+    {
+        this.load.image('testmap', 'assets/tilemaps/tutorial.png');
+        this.load.image('character', 'assets/sprites/testcharacter.png');
+    }
+
+    create ()
+    {
+        //  Set the camera and physics bounds to be the size of 4x4 bg images
+        this.cameras.main.setBounds(0, 0, 1920 * 2, 1080 * 2);
+        this.physics.world.setBounds(0, 0, 1920 * 2, 1080 * 2);
+
+        //  Mash 4 images together to create our background
+        this.add.image(0, 0, 'bg').setOrigin(0);
+        this.add.image(1920, 0, 'bg').setOrigin(0).setFlipX(true);
+        this.add.image(0, 1080, 'bg').setOrigin(0).setFlipY(true);
+        this.add.image(1920, 1080, 'bg').setOrigin(0).setFlipX(true).setFlipY(true);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.player = this.physics.add.image(400, 300, 'block');
+
+        this.player.setCollideWorldBounds(true);
+
+        this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
+    }
+
+    update ()
+    {
+        this.player.setVelocity(0);
+
+        if (this.cursors.left.isDown)
+        {
+            this.player.setVelocityX(-500);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.setVelocityX(500);
+        }
+
+        if (this.cursors.up.isDown)
+        {
+            this.player.setVelocityY(-500);
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.player.setVelocityY(500);
+        }
+    }
 }
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'park-world',
+    physics: {
+        default: 'arcade',
+    },
+    scene: [ Tutorial ]
+};
+
+const game = new Phaser.Game(config);
